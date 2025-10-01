08ac82438fc9e54803601ee0e721f0aa38f76935
Were Samson Bruno <werefast2000@gmail.com>
2025-10-01 13:14:50 +0300

    feat: add MetaPixel and FBPageView components for Facebook tracking

---
+++ my-app/components/MetaPixel.tsx
@@ -0,0 +1,34 @@
+// my-app/components/MetaPixel.tsx
+import Script from "next/script"
+
+export default function MetaPixel() {
+  const id = process.env.NEXT_PUBLIC_META_PIXEL_ID
+  if (!id) return null
+
+  const code = `
+    !function(f,b,e,v,n,t,s){
+      if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)
+    }(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '${id}');
+    fbq('track', 'PageView');
+  `
+
+  return (
+    <>
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{ __html: code }}
+      />
+      <noscript dangerouslySetInnerHTML={{
+        __html: `<img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=${id}&ev=PageView&noscript=1" />`
+      }} />
+    </>
+  )
+}
