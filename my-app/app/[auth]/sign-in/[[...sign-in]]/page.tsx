a0315f3a81608c341946ba161f0cc483baa748e5
Were Samson Bruno <werefast2000@gmail.com>
2026-03-28 10:51:05 +0300

    Make changes to fix captcha issues

--- my-app/app/[auth]/sign-in/[[...sign-in]]/page.tsx
+++ my-app/app/[auth]/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,19 @@
+'use client';
+
 import { SignIn } from "@clerk/nextjs";
 import { Navbar } from '@/components/navbar';
+import { AuthErrorBoundary } from '@/components/auth-error-boundary';
+import { usePathname } from 'next/navigation';
 
 export default function Page() {
+  const pathname = usePathname();
+
   return (
     <div className="flex justify-center py-24">
-         <Navbar />
-      <SignIn />
+      <Navbar />
+      <AuthErrorBoundary>
+        <SignIn key={pathname} />
+      </AuthErrorBoundary>
     </div>
   );
 }
